0daf45da58eebcae9c28a7c38c95418e718cb7c6
Peter Vieting <peter.vieting@rwth-aachen.de>
2023-03-05 14:23:06 +0100

    general table: WERs for all subsets template

--- tables/create_tables.sh
+++ tables/create_tables.sh
@@ -7,7 +7,7 @@ else
     EXCEL2LATEX=/u/vieting/Documents/text/excel-to-latex/excel_to_latex/excel_to_latex.py
 fi
 
-python ${EXCEL2LATEX} --file_in features.xlsx --all_lines --vertical_merge --sheet general --wer_cols E
+python ${EXCEL2LATEX} --file_in features.xlsx --all_lines --vertical_merge --sheet general --wer_cols E F G H
 python ${EXCEL2LATEX} --file_in features.xlsx --all_lines --vertical_merge --sheet pretraining --wer_cols C
 python ${EXCEL2LATEX} --file_in features.xlsx --all_lines --vertical_merge --sheet window_size --wer_cols C
 python ${EXCEL2LATEX} --file_in features.xlsx --vertical_merge --sheet scf_size --wer_cols E
